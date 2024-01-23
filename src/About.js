f1d6f35f30bcebb406a5c05834cb50d41100a254
rmdhnz <ramadhanfadhly2@gmail.com>
2024-01-23 21:30:52 +0700

    adding technology that I used

--- src/About.js
+++ src/About.js
@@ -1,5 +1,22 @@
 import React from "react";
-import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaGit } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaGitAlt,
+  FaBootstrap,
+  FaFigma,
+  FaPython,
+  FaPhp,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiCodeigniter,
+  SiWebpack,
+  SiMysql,
+} from "react-icons/si";
 export default function About() {
   return (
     <section className="w-full dark:bg-slate-900 flex px-8 py-32 justify-center">
@@ -11,12 +28,27 @@ export default function About() {
               Your Network is Your{" "}
               <span className="text-slate-900 dark:text-white">Net Worth</span>
             </h3>
-            <p className="text-justify font-poppins mt-3 dark:text-white">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro ad
-              reiciendis voluptatibus amet eveniet aperiam earum consequuntur
-              quibusdam veniam in tenetur non doloremque excepturi aspernatur,
-              sunt sit autem saepe totam!
+            <p className="text-justify font-poppins my-3 dark:text-white">
+              An ITS Electrical Engineering student from Palembang, South
+              Sumatra. Developing websites both front end and backend. Also
+              interested in learning machine learning such as Neural Network,
+              Fuzzy, Computer Vision, etc
             </p>
+            <div className="dark:text-white flex flex-wrap gap-2 justify-around text-3xl">
+              <FaHtml5 />
+              <FaCss3 />
+              <FaJs />
+              <FaBootstrap />
+              <SiTailwindcss />
+              <SiCodeigniter />
+              <FaPython />
+              <FaPhp />
+              <FaReact />
+              <FaFigma />
+              <SiMysql />
+              <FaGitAlt />
+              <SiWebpack />
+            </div>
           </div>
           <div className="mt-5 w-full px-4 md:w-1/2">
             <h1 className="font-poppins text-2xl font-bold dark:text-white">
